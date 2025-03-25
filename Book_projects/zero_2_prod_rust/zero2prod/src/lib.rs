800857287e52ee4f62c2a74d232422301ceea41f
Gisleudo <gisleudo.cmf@gmail.com>
2025-03-25 10:24:22 -0300

    Change structure of project to support a library - changes up to page 59

---
+++ Book_projects/zero_2_prod_rust/zero2prod/src/lib.rs
@@ -0,0 +1,22 @@
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+#[tokio::main]
+pub async fn run() -> Result<(), std::io::Error> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/{name}", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
+    })
+    .bind("127.0.0.1:7979")?
+    .run()
+    .await
+}
