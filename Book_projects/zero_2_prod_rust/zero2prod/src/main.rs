244d831220c70b6bd7b6dc96df355dac3dcdc3c8
Gisleudo <gisleudo.cmf@gmail.com>
2025-04-01 15:01:53 -0300

    Add integration check for forms with actix - up to page 80

--- Book_projects/zero_2_prod_rust/zero2prod/src/main.rs
+++ Book_projects/zero_2_prod_rust/zero2prod/src/main.rs
@@ -4,7 +4,7 @@ use zero2prod::run;
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    let port = listener.local_addr().unwrap().port();
-    println!("{port}");
+    let address = listener.local_addr().unwrap();
+    println!("runing server on: http://{address}");
     run(listener)?.await
 }
--- Book_projects/zero_2_prod_rust/zero2prod/tests/health_check.rs
+++ Book_projects/zero_2_prod_rust/zero2prod/tests/health_check.rs
@@ -37,7 +37,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
     let response = client
         .post(format!("{}/subscriptions", &app_address))
-        .header("Content-Type", "application/x-www-from-urlencoded")
+        .header("Content-Type", "application/x-www-form-urlencoded")
         .body(body)
         .send()
         .await
@@ -53,7 +53,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
     let client = reqwest::Client::new();
     let test_cases = vec![
         ("name=le%20guin", "missing the email"),
-        ("email=ursula_le_guin%40gmail.com", "missing name"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
         ("", "missing both name and email"),
     ];
 
@@ -61,7 +61,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
         //Act
         let response = client
             .post(format!("{}/subscriptions", &app_address))
-            .header("Content-Type", "x-www-from-urlencoded")
+            .header("Content-Type", "application/x-www-form-urlencoded")
             .body(invalid_body)
             .send()
             .await
