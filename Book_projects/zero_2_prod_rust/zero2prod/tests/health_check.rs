43c0a099062fe7648b28936ddc91ac171ca9bf61
Gisleudo <gisleudo.cmf@gmail.com>
2025-04-08 15:05:19 -0300

    Complete steps up to page 115

--- Book_projects/zero_2_prod_rust/zero2prod/tests/health_check.rs
+++ Book_projects/zero_2_prod_rust/zero2prod/tests/health_check.rs
@@ -1,6 +1,7 @@
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
-
-use reqwest::Client;
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
 
 #[tokio::test]
 async fn health_check_works() {
@@ -22,7 +23,7 @@ async fn health_check_works() {
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let server = run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
     format!("http://127.0.0.1:{port}")
 }
@@ -31,6 +32,11 @@ fn spawn_app() -> String {
 async fn subscribe_returns_a_200_for_valid_form_data() {
     //Arrange
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to postgres");
     let client = reqwest::Client::new();
 
     //Act
@@ -44,6 +50,13 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .expect("Failed to execute request.");
     //Assert
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
