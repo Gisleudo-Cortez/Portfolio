8e4901f252ac0ebf6c7b1bd37af475fcfe70ee82
Gisleudo <gisleudo.cmf@gmail.com>
2025-04-11 15:35:26 -0300

    Complete up to page - 158 -- add basic logging

--- Book_projects/zero_2_prod_rust/zero2prod/src/main.rs
+++ Book_projects/zero_2_prod_rust/zero2prod/src/main.rs
@@ -1,3 +1,4 @@
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
@@ -6,6 +7,7 @@ use zero2prod::startup::run;
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     // Panic if we can't read configuration
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection = PgPool::connect(&configuration.database.connection_string())
         .await
