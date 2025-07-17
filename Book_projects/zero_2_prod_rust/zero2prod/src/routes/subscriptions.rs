6ed09239b25531d62109b47ddaa126fcfa298d98
Gisleudo Cortez <gisleudo.cmf@gmail.com>
2025-07-17 14:27:50 -0300

    Complete implementation of email validation and error types, changes up to page 301

--- Book_projects/zero_2_prod_rust/zero2prod/src/routes/subscriptions.rs
+++ Book_projects/zero_2_prod_rust/zero2prod/src/routes/subscriptions.rs
@@ -1,4 +1,4 @@
-use crate::domain::{NewSubscriber, SubscriberName};
+use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
 use actix_web::{HttpResponse, web};
 use chrono::Utc;
 use sqlx::PgPool;
@@ -23,8 +23,12 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         Ok(name) => name,
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
+    let email = match SubscriberEmail::parse(form.0.email) {
+        Ok(email) => email,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
     let new_subscriber = NewSubscriber {
-        email: form.0.email,
+        email: email,
         name: name,
     };
 
@@ -45,7 +49,7 @@ pub async fn insert_subscriber(
     sqlx::query!(
         r#"INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES($1, $2, $3, $4)"#,
         Uuid::new_v4(),
-        new_subscriber.email,
+        new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
         Utc::now()
     )
